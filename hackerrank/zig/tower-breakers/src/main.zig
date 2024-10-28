1f06419675f0f38bc68be9752dbf916ea180e131
Andrew Jarrett <andrew.jarrett@gatech.edu>
2024-10-27 20:39:52 -0400

    Fix input handling in main.zig

--- hackerrank/zig/tower-breakers/src/main.zig
+++ hackerrank/zig/tower-breakers/src/main.zig
@@ -41,48 +41,57 @@ pub fn main() !void {
     assert(t <= 100);
     for (0..t) |_| {
         isValidInput = false;
-        var n: u32 = undefined;
-        var m: u32 = undefined;
-        while (!isValidInput) {
-            const n_str = (reader.readUntilDelimiterOrEof(&buf, ' ') catch {
+        var maybeN: ?u32 = null;
+        var maybeM: ?u32 = null;
+        retry: while (!isValidInput) {
+            const nm_line = (reader.readUntilDelimiterOrEof(&buf, '\n') catch {
                 std.debug.print("Unable to read your input, please try again:\n", .{});
                 continue;
             }) orelse "0";
 
-            n = std.fmt.parseInt(u32, n_str, 0) catch {
-                std.debug.print("Unable to parse your input as a number, please try again:\n", .{});
-                continue;
-            };
+            var split = std.mem.split(u8, nm_line, " ");
+            while (split.next()) |c| {
+                if (maybeN == null) {
+                    maybeN = std.fmt.parseInt(u32, c, 0) catch {
+                        std.debug.print("Unable to parse your input as a number, please try again:\n", .{});
+                        continue :retry;
+                    };
 
-            if (1 > n or n > 1000000) {} else {
-                std.debug.print("Please input a number between 1 and 1000000.\n", .{});
-                continue;
+                    if (1 > maybeN.? or maybeN.? > 1000000) {
+                        std.debug.print("Please input a number between 1 and 1000000.\n", .{});
+                        continue :retry;
+                    }
+                } else if (maybeM == null) {
+                    maybeM = std.fmt.parseInt(u32, c, 0) catch {
+                        std.debug.print("Unable to parse your input as a number, please try again:\n", .{});
+                        continue :retry;
+                    };
+
+                    if (1 <= maybeM.? and maybeM.? <= 1000000) {
+                        isValidInput = true;
+                        break;
+                    } else {
+                        std.debug.print("Please input a number between 1 and 1000000.\n", .{});
+                        continue :retry;
+                    }
+                }
             }
 
-            const m_str = (reader.readUntilDelimiterOrEof(&buf, ' ') catch {
-                std.debug.print("Unable to read your input, please try again:\n", .{});
-                continue;
-            }) orelse "0";
-
-            m = std.fmt.parseInt(u32, m_str, 0) catch {
-                std.debug.print("Unable to parse your input as a number, please try again:\n", .{});
-                continue;
-            };
-
-            if (1 <= m and m <= 1000000) {
+            if (maybeN != null and maybeM != null) {
                 isValidInput = true;
             } else {
-                std.debug.print("Please input a number between 1 and 1000000.\n", .{});
-                continue;
+                std.debug.print("Please provide two numbers for 'n' and 'm'.\n", .{});
             }
         }
 
+        const n = maybeN.?;
+        const m = maybeM.?;
         assert(1 <= n);
         assert(n <= 1000000);
         assert(1 <= m);
         assert(m <= 1000000);
 
-        try writer.print("{d} ", .{Solution.towerBreakers(n, m)});
+        try writer.print("{d}\n", .{Solution.towerBreakers(n, m)});
     }
 
     try bw.flush();
